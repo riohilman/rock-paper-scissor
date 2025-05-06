e1b7a0f17f53186bf3287900fc63111abc3011d6
Rio Hilman <53969666+riohilman@users.noreply.github.com>
2025-05-06 18:00:55 +0700

    Add global score variables and playRound function

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 function getComputerChoice() {
-    let computerChoice;
     let number = Math.random();
-    if (number < 0.33) {
-        return computerChoice = "Rock";
-    } else if (number < 0.67) {
-        return computerChoice = "Paper";
+    if (number < 1/3) {
+        return "rock";
+    } else if (number < 2/3) {
+        return "paper";
     } else {
-        return computerChoice = "Scissor";
+        return "scissors";
     }
 }
 
 function getHumanChoice() {
-    return prompt("Enter your choice")
+    return prompt("Enter your choice: rock, paper or scissors")
 }
 
-console.log(getHumanChoice())
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice == computerChoice) {
+        console.log("It's a tie")
+    } else if (
+        (humanChoice == "rock" && computerChoice == "scissors") ||
+        (humanChoice == "paper" && computerChoice == "rock") ||
+        (humanChoice == "scissors" && computerChoice == "paper")
+    ) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    } else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+}
